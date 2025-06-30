eee45dee23973173c1c7ec6ca3b2d1cd548191f3
Ghanshyambunkar <ghanshyambunkar016@gmail.com>
2025-06-30 22:43:56 +0530

    update

--- backend/router/productRoute.js
+++ backend/router/productRoute.js
@@ -1,17 +1,41 @@
-const express=require("express");
-const { getAllProducts ,createProduct, updateProduct, deleteProduct, getProductDetails} = require("../controllers/productController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const express = require("express");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
+  getAdminProducts,
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-const router=express.Router();
+const router = express.Router();
 
-router.route("/products").get(getAllProducts); //isAuthanthicate is add
+router.route("/products").get(getAllProducts);
 
-router.route("/products/new").post(createProduct);
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
-router.route("/products/:id").put(updateProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
-router.route("/products/:id").delete(deleteProduct);
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
-router.route("/products/:id").get(getProductDetails);
+router.route("/product/:id").get(getProductDetails);
 
-module.exports=router;
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
+
+module.exports = router;
